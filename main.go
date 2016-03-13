c759b61eaa59f6d3e2446581a69de2b5b0695858
Shiro <final@fantasy.pe>
2016-03-13 19:17:28 +0800

    Add version info

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+//go:generate goversioninfo -icon=icon.ico
+
 // EVEModX Main
 // Works under Microsoft Windows platform
 // Still under HEAVY developing
@@ -28,7 +30,7 @@ import (
 )
 
 const (
-	VERSION = "0.0.2"
+	VERSION = "0.0.1a"
 )
 
 
